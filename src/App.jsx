2bc0ea7ed7d58690a099ca909d77d3288ddb73cc
sangjun <987654321123sin@gmail.com>
2024-06-10 18:17:38 +0900

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,16 @@
-import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
-import { decrement, increment } from "./redux/slices/calculatorSlice";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
-// TODO: 계산결과를 redux를 이용한 전역상태로 관리해 보세요.
+// TODO: todos 상태를 리덕스를 이용한 전역상태로 관리한 투두리스트를 작성해 보세요.
 function App() {
-  const count = useSelector((state) => state.counter.value);
-  const dispatch = useDispatch();
-
   return (
-    <div className="App">
-      <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
-      <div>
-        <input /> 만큼을
-        <button
-          onClick={() => {
-            dispatch(increment());
-          }}
-        >
-          더할게요
-        </button>
-        <button
-          onClick={() => {
-            dispatch(decrement());
-          }}
-        >
-          뺄게요
-        </button>
-      </div>
-      <hr />
-      <div>
-        <h3>결과</h3>
-        <p>{count}</p>
-      </div>
-    </div>
+    <>
+      <h1>투두리스트 타임어택</h1>
+      <TodoForm />
+      <TodoList isDone={false} />
+      <TodoList isDone={true} />
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import configStore from "./redux/config/configStore.js";
+import configStore from "./redux/config/configStore";
 
 // TODO: redux store를 모든 App에 공유할 수 있게 하세요.
 ReactDOM.createRoot(document.getElementById("root")).render(
